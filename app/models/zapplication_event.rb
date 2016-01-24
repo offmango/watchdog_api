fc7ca0c527541d8f841669ff2ffd0c6a5fe11452
Eric Kurzenberger <info@etk.me>
2016-01-24 16:09:25 -0500

    add start_time, end_time methods

--- app/models/zapplication_event.rb
+++ app/models/zapplication_event.rb
@@ -1,5 +1,19 @@
 class ZapplicationEvent < ActiveRecord::Base
 
   self.table_name = "ZAPPLICATION"
-  
+
+  def start_time 
+    converted_time(self.ZSTARTDATE)
+  end
+
+  def end_time
+    converted_time(self.ZENDDATE)
+  end
+
+  private
+
+  def converted_time(time_integer)
+    Time.at(time_integer) + 31.years + 1.day rescue nil
+  end
+
 end
